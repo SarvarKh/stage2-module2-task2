83a1041ba2b6bb2649dff423c4c024d8b82d6feb
Sarvar Khalimov <khalimovsarvar@gmail.com>
2023-02-11 20:42:40 +0500

    Override contectInitialized method in ContextListener

--- src/main/java/com/example/listener/ContextListener.java
+++ src/main/java/com/example/listener/ContextListener.java
@@ -1,5 +1,14 @@
 package com.example.listener;
 
-public class ContextListener {
-    //write your code here!
+import javax.print.attribute.standard.DateTimeAtCreation;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.time.LocalDateTime;
+
+public class ContextListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        LocalDateTime servletTimeInit = LocalDateTime.now();
+        sce.getServletContext().setAttribute("servletTimeInit", servletTimeInit);
+    }
 }
