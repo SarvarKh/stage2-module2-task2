291ff687f896614c611c41282e2ff57170bd9970
Sarvar Khalimov <khalimovsarvar@gmail.com>
2023-02-11 19:04:43 +0500

    Add AuthFilter and doGet method to LoginServlet

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -1,5 +1,35 @@
 package com.example.filter;
 
-public class AuthFilter {
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/user/*")
+public class AuthFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        if (request.getRequestURI().startsWith("/user")) {
+            HttpSession session = request.getSession();
+            if (session.getAttribute("user") == null) {
+                RequestDispatcher dispatcher = request.getRequestDispatcher("/login.jsp");
+                dispatcher.forward(request, servletResponse);
+            }
+        }
+
+        filterChain.doFilter(request, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
     //write your code here!
 }
--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -37,4 +37,9 @@ public class LoginServlet extends HttpServlet {
             dispatcher.forward(req, resp);
         }
     }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }
